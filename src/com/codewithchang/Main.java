d90911c8a6b40ae860043dab118b417dbf6d95b3
Chang <cnxiong03@gmail.com>
2020-12-28 13:55:00 -0500

    Char and boolean and how they work

---
+++ src/com/codewithchang/Main.java
@@ -0,0 +1,19 @@
+package com.codewithchang;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyRight = '\u00A9';
+        System.out.println(myCopyRight);
+
+        boolean myTureBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
